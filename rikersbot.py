56e2d59e4f2fa7b54c2d51e38d4b99db9b67195b
denten <denten@gmail.com>
2015-10-09 16:45:33 -0400

    adding the log file

--- rikersbot.py
+++ rikersbot.py
@@ -15,40 +15,38 @@ def creds():
         return row['token'], row['secret'], row['akey'], row['asecret']
 
 # select line number at random, check against what has been twitted
-def random():
+def random_text():
     # prune according to the "already tweeted" index log
 
     # generate a random number in the remaining range
 
     # pass the text of the tweet
 
-    # update the "already tweeted" index log
+    with open(path + 'tweets.txt', 'r') as f:
+        tweets = f.readlines()
 
-def text():
-    now = datetime.datetime.now().time()
-    # no longer a csv! fix this
-    with open(path + 'tweets.txt', 'r') as csvfile:
-        tweets = csv.DictReader(csvfile, delimiter=",")
-        for row in tweets:
-            ft = row['time']
-            hour = int(ft.split(':')[0])
-            minute = int(ft.split(':')[1])
-            if hour == now.hour and minute == now.minute:
-                return row['text']
+    tweet = tweets[ind]
 
-def tweet(k, t):
-    try:
-        # k stores token, secret, api key, and api secret
-        auth = tweepy.OAuthHandler(k[2], k[3])
-        auth.set_access_token(k[0], k[1])
-        api = tweepy.API(auth)
-        api.update_status(t)
-        # dummy write to a file instead of tweeting
-        # f = open(path + 'log.txt', 'a')
-        # f.write(t + '\n')
-    except tweepy.error.TweepError, e:
-        # implement logging later
-        # print 'failed because of %s' % e.reason
-        pass
+    # append "already tweeted" to index log
+    with open(path + 'log.csv', a) as f:
+        f.write(ind)
+
+def tweet(t):
+
+# def tweet(k, t):
+#     try:
+#         # k stores token, secret, api key, and api secret
+#         auth = tweepy.OAuthHandler(k[2], k[3])
+#         auth.set_access_token(k[0], k[1])
+#         api = tweepy.API(auth)
+#         api.update_status(t)
+#         # dummy write to a file instead of tweeting
+#         # f = open(path + 'log.txt', 'a')
+#         # f.write(t + '\n')
+#     except tweepy.error.TweepError, e:
+#         # implement logging later
+#         # print 'failed because of %s' % e.reason
+#         pass
+# 
 
 tweet(creds(), text())
