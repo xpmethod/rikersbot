6d5e0c1b1d5e9d628492f18eeed0fe2b3de13e27
Dennis Tenen <denten@gmail.com>
2015-10-22 21:01:55 +0000

    updating rikersbot

--- rikersbot.py
+++ rikersbot.py
@@ -6,7 +6,7 @@ import random
 # cron settings
 # * * 12 03 * python /home/shared-drives/litclock/cron-bot.py
 # paths have to be explicit for cron
-path = '/home/shared-drives/litclock/'
+path = '/home/denten/bots/rikersbot/'
 
 # separate the credits out to keep out of github
 def creds():
@@ -18,12 +18,12 @@ def creds():
 # select line number at random, check against what has been twitted
 def text():
     # read in the forbidden list and cast to int
-    with open('forbidden.txt', 'r') as f:
+    with open(path + 'forbidden.txt', 'r') as f:
         flist = f.readlines()
         forbid_list = [int(i) for i in flist]
 
     # open tweets get length
-    with open('tweets.txt', 'r') as f:
+    with open(path + 'tweets.txt', 'r') as f:
         tweets_list = f.readlines()
         tweets_range = len(tweets_list) - 1
 
@@ -35,31 +35,26 @@ def text():
         tweet_index = random.randint(0,tweets_range)
     else:
         # append to the forbidden list and return the tweet
-        with open('forbidden.txt', 'a') as f:
+        with open(path + 'forbidden.txt', 'a') as f:
              f.write(str(tweet_index) + '\n')
 
         # print tweet_index
         return tweets_list[tweet_index]
 
-def tweet(t):
-    print t
+def tweet(k, t):
+try:
+         # k stores token, secret, api key, and api secret
+         auth = tweepy.OAuthHandler(k[2], k[3])
+         auth.set_access_token(k[0], k[1])
+         api = tweepy.API(auth)
+         api.update_status(t)
+         # dummy write to a file instead of tweeting
+         # f = open(path + 'log.txt', 'a')
+         # f.write(t + '\n')
+     except tweepy.error.TweepError, e:
+         # implement logging later
+         # print 'failed because of %s' % e.reason
+         pass
 
-tweet(text())
 
-# def tweet(k, t):
-#     try:
-#         # k stores token, secret, api key, and api secret
-#         auth = tweepy.OAuthHandler(k[2], k[3])
-#         auth.set_access_token(k[0], k[1])
-#         api = tweepy.API(auth)
-#         api.update_status(t)
-#         # dummy write to a file instead of tweeting
-#         # f = open(path + 'log.txt', 'a')
-#         # f.write(t + '\n')
-#     except tweepy.error.TweepError, e:
-#         # implement logging later
-#         # print 'failed because of %s' % e.reason
-#         pass
-#
-
-# tweet(creds(), text())
+ tweet(creds(), text())
