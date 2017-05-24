0a259c8b13a6dd802e6f52c534ee795e1de0a001
denten <denten@gmail.com>
2017-05-24 16:17:23 -0400

    upload the tweets

--- length-test.py
+++ length-test.py
@@ -1,6 +1,6 @@
 with open('tweets.txt', 'r') as f:
     for line in f:
         # chomp the end of line character
-        if len(line.rstrip()) > 140:
+        if len(line.rstrip()) > 139:
             print len(line)
             print line
